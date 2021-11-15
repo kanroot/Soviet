c10bdbe9f0a020703ee7b8fac4837bb080d2aa42
kan <root.kan.ss@gmail.com>
2021-11-15 20:55:19 -0300

    fix

--- Src/Soviet/Debug/DebugNode.cs
+++ Src/Soviet/Debug/DebugNode.cs
@@ -6,7 +6,7 @@ namespace Soviet.Soviet.Debug
 	public class DebugNode : Control
 	{
 		private Spatial cameraElevation;
-		private Spatial cameraGimball;
+		private Spatial cameraGimbal;
 		[Export] private NodePath cameraPath;
 		private Label cameraPos;
 		private Array countCells;
@@ -24,15 +24,15 @@ namespace Soviet.Soviet.Debug
 
 		public override void _Ready()
 		{
-			cameraGimball = GetNode<Spatial>(cameraPath);
+			cameraGimbal = GetNode<Spatial>(cameraPath);
 			floor = GetNode<GridMap>(floorPath);
 			GetNodes();
 		}
 
 		public override void _Process(float delta)
 		{
-			var x = cameraGimball.Translation.x;
-			var z = cameraGimball.Translation.z;
+			var x = cameraGimbal.Translation.x;
+			var z = cameraGimbal.Translation.z;
 			cameraPos.Text = $"Posicion:({x.ToString()} , {z.ToString()})";
 			elevation.Text = $"Elevacion: {cameraElevation.Translation.y.ToString()}";
 			textCountCells.Text = $"Cuenta de celdas: {countCells.Count.ToString()}";
@@ -44,7 +44,7 @@ namespace Soviet.Soviet.Debug
 		public void GetNodes()
 		{
 			vBoxContainer = GetChild<VBoxContainer>(0);
-			cameraElevation = cameraGimball.GetChild<Spatial>(0);
+			cameraElevation = cameraGimbal.GetChild<Spatial>(0);
 			cameraPos = vBoxContainer.GetChild<Label>(0);
 			elevation = vBoxContainer.GetChild<Label>(1);
 			textCountCells = vBoxContainer.GetChild<Label>(2);
