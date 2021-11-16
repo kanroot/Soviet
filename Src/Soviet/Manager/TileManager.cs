515d4dedbaaa289bb8638bb1033848883552cea2
kan <root.kan.ss@gmail.com>
2021-11-16 16:23:37 -0300

    Limpieza general

--- Src/Soviet/Manager/TileManager.cs
+++ Src/Soviet/Manager/TileManager.cs
@@ -5,31 +5,26 @@ namespace Soviet.Soviet.Manager
 {
 	public class TileManager : Node
 	{
-		private static TileManager instance;
-		public static TileManager Instance => instance;
+		private Control debug;
 
 		private GridMap floor;
 		private GridMap tree;
-		private Control debug;
-		
+		public static TileManager Instance { get; private set; }
+
 		public override void _Ready()
 		{
-			instance = this;
+			Instance = this;
 			floor = GetTree().CurrentScene.FindNode("Floor") as GridMap;
 			tree = GetTree().CurrentScene.FindNode("Objects") as GridMap;
 			debug = GetTree().CurrentScene.FindNode("DebugNode") as Control;
 		}
-		
+
 		public void CreateTileAt(int tile, Vector3 pos, int layer)
 		{
-			if ((GridMapLayer) layer == GridMapLayer.Floor)
-			{
+			if ((GridMapLayer)layer == GridMapLayer.Floor)
 				floor.SetCellItem((int)pos.x, (int)pos.y, (int)pos.z, tile);
-			}
 			else
-			{
 				tree.SetCellItem((int)pos.x, (int)pos.y, (int)pos.z, tile);
-			}
 
 			var a = debug as DebugNode;
 			a?.GetNodes();
@@ -39,7 +34,6 @@ namespace Soviet.Soviet.Manager
 		{
 			return floor.MapToWorld((int)to.x, (int)to.y, (int)to.z);
 		}
-
 	}
 
 
@@ -47,7 +41,7 @@ namespace Soviet.Soviet.Manager
 	{
 		Grass,
 		River,
-		Ground 
+		Ground
 	}
 
 	public enum TileConstTree
